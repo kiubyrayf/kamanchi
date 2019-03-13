967b123cdedb7d7db3c066ed5d7309363a257173
Rafael Alejandro Velez Garcia <rafael velez@tiempodevelopment.com>
2019-03-13 15:57:26 -0600

    added new pages to the app

---
+++ src/app/topic-selection/topic-selection.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-topic-selection',
+  templateUrl: './topic-selection.component.html'
+})
+export class TopicSelectionComponent implements OnInit {
+
+  xLenght = 50;
+  yLenght = 50;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
